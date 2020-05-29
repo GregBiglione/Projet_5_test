8d9ab9c31311638dcc40efea44d86e0011d248ef
GregBiglione <gregory.biglione81@gmail.com>
2020-05-29 21:51:12 +0200

    Création du Recycler View et des items

--- app/src/main/java/com/greg/todoc/service/DummyTaskService.java
+++ app/src/main/java/com/greg/todoc/service/DummyTaskService.java
@@ -11,8 +11,8 @@ public abstract class DummyTaskService {
 
     public static List<Task> DUMMY_TASK = Arrays.asList(
             new Task(1, "rose", "Nettoyer les vitres", new Date(24/05/2020) , "Projet Lucidia"),
-            new Task(2, "vert", "Vider le lave vaisselle", new Date(28/05/2020), "Projet Circu"),
-            new Task(3, "vert", "Passer l'aspirateur", new Date(3/06/2020), "Projet Circu"),
+            new Task(2, "vert", "Vider le lave vaisselle", new Date(28/05/2020), "Projet Circus"),
+            new Task(3, "vert", "Passer l'aspirateur", new Date(3/06/2020), "Projet Circus"),
             new Task(4, "rose", "Arroser les plantes", new Date(5/06/2020), "Projet Lucidia"),
             new Task(5, "bleu", "Nettoyer les toilettes", new Date(6/06/2020), "Projet Tartampion")
     );
