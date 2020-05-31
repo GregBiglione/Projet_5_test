c6204d9dc7c1fa593faf02ad165a9f11c75ebe5b
GregBiglione <gregory.biglione81@gmail.com>
2020-05-31 21:35:02 +0200

    Ajout du Spinner peuplé dans la Project Dialog Box

--- app/src/main/java/com/greg/todoc/dialog_box/ProjectDialog.java
+++ app/src/main/java/com/greg/todoc/dialog_box/ProjectDialog.java
@@ -5,19 +5,30 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
 import com.greg.todoc.R;
 
+import butterknife.BindView;
+
 public class ProjectDialog extends AppCompatDialogFragment {
+
+    @BindView(R.id.projectDialogSpinner) Spinner mSpinner;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.layout_project_dialog, null);
+        mSpinner = view.findViewById(R.id.projectDialogSpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.project_array));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(adapter);
         builder.setView(view)
                 .setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
             @Override
