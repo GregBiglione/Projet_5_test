335880cfbc0137fc93bbcb2a7548a45b0996d0c2
GregBiglione <gregory.biglione81@gmail.com>
2020-06-21 03:47:39 +0200

    Création de TaskDao

---
+++ app/src/main/java/com/greg/todoc/database/dao/TaskDao.java
@@ -0,0 +1,23 @@
+package com.greg.todoc.database.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.greg.todoc.model.Task;
+
+import java.util.List;
+
+@Dao
+public interface TaskDao {
+
+    @Query("SELECT * FROM Task WHERE id = :id")
+    LiveData<List<Task>>getTasks(long id);
+
+    @Insert
+    long insertTask(Task task);
+
+    @Query("DELETE FROM Task WHERE id = :id")
+    int deleteTask(long id);
+}
